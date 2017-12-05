689127e7122a91fa97e7bf428cf8d281e38f7e3f
胡庆渊 <huqingyuan@pc-207-177.customer.iqleeds.quintain.lan>
2017-12-05 11:11:36 +0000

    build GUI and create files of the first part(DC-DC converter)

---
+++ power electronic calculater/TableViewController1.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController1.h
+//  power electronic calculater
+//
+//  Created by 胡庆渊 on 2017/12/5.
+//  Copyright © 2017年 Univercity of leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController1 : UITableViewController
+
+@end
---
+++ power electronic calculater/ViewController1_1.m
@@ -0,0 +1,37 @@
+//
+//  ViewController1_1.m
+//  power electronic calculater
+//
+//  Created by 胡庆渊 on 2017/12/5.
+//  Copyright © 2017年 Univercity of leeds. All rights reserved.
+//
+
+#import "ViewController1_1.h"
+
+@interface ViewController1_1 ()
+
+@end
+
+@implementation ViewController1_1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
