42ae06c271f08d4d7fda04a35f6f313f4157ff8c
胡庆渊 <huqingyuan@pc-207-177.customer.iqleeds.quintain.lan>
2017-12-05 11:14:54 +0000

    the file of the part 2(AC power factor)

---
+++ power electronic calculater/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  power electronic calculater
+//
+//  Created by 胡庆渊 on 2017/12/5.
+//  Copyright © 2017年 Univercity of leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
