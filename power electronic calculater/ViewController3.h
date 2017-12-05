f57fdf9d8316f7bfbcf361f5a59c41e19866848f
胡庆渊 <huqingyuan@pc-207-177.customer.iqleeds.quintain.lan>
2017-12-05 11:16:36 +0000

    the files of the part3(DC-AC converter)

---
+++ power electronic calculater/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  power electronic calculater
+//
+//  Created by 胡庆渊 on 2017/12/5.
+//  Copyright © 2017年 Univercity of leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIViewController
+
+@end
